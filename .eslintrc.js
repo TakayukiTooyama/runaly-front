4ec3a2f022a593ccec2865cfb0de01a72b6d9a18
TakayukiTooyama <t.tooyama107@gmail.com>
2023-09-29 16:29:33 +0900

    :tada: 初期環境構築完了

---
+++ .eslintrc.js
@@ -0,0 +1,67 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['import'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:tailwindcss/recommended',
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling'],
+          'object',
+          'type',
+          'index',
+        ],
+        'newlines-between': 'always',
+        pathGroupsExcludedImportTypes: ['builtin'],
+        pathGroups: [
+          {
+            pattern: '@/utils/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/libs/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/hooks/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/components/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/const/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/types/**',
+            group: 'internal',
+            position: 'before',
+          },
+        ],
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ],
+  },
+}
